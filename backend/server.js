f09cfe96d1c6f4000b0d2ee300d12374fb8f2609
Letsawake <nikolashspark@gmail.com>
2025-10-04 21:41:24 +0300

    Fix Apify input format for Facebook Ads Scraper

--- backend/server.js
+++ backend/server.js
@@ -178,9 +178,17 @@ app.post('/api/apify/facebook-ads', async (req, res) => {
     // Запускаємо Apify Actor через HTTP API
     // Використовуємо правильний Actor ID з тильдою
     const actorId = 'apify~facebook-ads-scraper';
+    
+    // Правильний формат input для Facebook Ads Scraper
     const input = {
-      startUrls: [`https://www.facebook.com/ads/library/?active_status=active&ad_type=all&country=${country}&view_all_page_id=${pageId}`],
-      maxItems: 5
+      queries: [
+        {
+          query: `page_id:${pageId}`,
+          country: country
+        }
+      ],
+      maxItems: 5,
+      adStatus: "ACTIVE"
     };
 
     console.log('Sending request to Apify API...');
